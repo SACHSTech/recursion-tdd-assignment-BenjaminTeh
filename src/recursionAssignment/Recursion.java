83b3bb12059f086b3016a912b6a552cc4ae2e6de
Benjamin Teh <>
2021-05-21 18:40:14 +0000

    stringClean Green

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -50,7 +50,10 @@ public class Recursion{
   }
 
   public static String stringClean(String str){
-    
+    if(str.length() == 1){
+      return str;
+    }
+
     if (str.charAt(0) == str.charAt(1)){
       return stringClean(str.substring(1));
     }
